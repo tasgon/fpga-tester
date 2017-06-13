e7bb8638c0606948a1554576f113599b7393a90d
tasgoon <tasgoon@outlook.com>
2017-06-12 23:53:12 -0400

    made it work

--- src/main.rs
+++ src/main.rs
@@ -2,24 +2,31 @@ extern crate memmap;
 
 use std::env;
 use memmap::{Mmap, Protection};
+use std::fs::File;
+use std::io::Write;
 
 #[cfg(target_arch = "arm")]
 fn write(val: u8) -> bool {
-    let map = Mmap::open_with_offset("/dev/mem", Protection::ReadWrite, 0xff200000, 4096).unwrap();
-    unsafe { map.as_mut_slice() }.write(value).unwrap();
+    let file = File::open("/dev/mem").unwrap();
+    let mut map = Mmap::open_with_offset(&file, Protection::ReadWrite, 0xff200000, 4096).unwrap();
+    let write_size = unsafe { map.as_mut_slice() }.write(&[val]).unwrap();
 
-    true
+    if write_size > 0 { true } else { false }
 }
 
 #[cfg(not(target_arch = "arm"))]
 fn write(val: u8) -> bool { false }
 
 fn main() {
+    let do_write = env::args().count() < 3; //don't write the value if there is more than one extra argument
     let value: u8 = env::args().last().unwrap().parse::<u8>().unwrap();
     println!("Val: {}", value);
     
-    match write(value) {
-        true => println!("byte written"),
-        false => println!("byte not written")
+    match do_write {
+        true => match write(value) {
+            true => println!("byte written"),
+            false => println!("byte not written")
+        },
+        false => println!("ignoring data")
     }
 }
